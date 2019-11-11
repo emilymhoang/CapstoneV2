51941427ebf3544228b43b5dbbee9417bf15149a
kk <labpatron@BUR251-1873-COB>
2019-11-11 18:59:30 -0500

    master

--- Index.aspx.cs
+++ Index.aspx.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class Index : System.Web.UI.Page
 {
+
+    SqlConnection sc = new SqlConnection(WebConfigurationManager.ConnectionStrings["RDSConnectionString"].ConnectionString);
     // Isaac was here
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -18,14 +22,49 @@ public partial class Index : System.Web.UI.Page
     }
     protected void myAccount(object sender, EventArgs e)
     {
+
         if (Session["LoggedIn"].ToString() == "true")
         {
-            Response.Redirect("TenantDashboard.aspx");
+
+            sc.Open();
+            int accountID = Convert.ToInt32(Session["accountID"]);
+            Response.Write(accountID);
+
+            SqlCommand login = new SqlCommand("SELECT HostID, TenantID, AdminID FROM [Capstone].[dbo].[Login] WHERE AccountID = @AccountID", sc);
+            login.Parameters.AddWithValue("@AccountID", accountID);
+            login.Connection = sc;
+            //login.ExecuteNonQuery();
+            SqlDataReader rdr = login.ExecuteReader();
+            string tenantID = "", hostID = "", adminID = "";
+            while (rdr.Read())
+            {
+                adminID = rdr["AdminID"].ToString();
+                Session["adminID"] = adminID;
+                hostID = rdr["HostID"].ToString();
+                Session["hostID"] = hostID;
+                tenantID = rdr["TenantID"].ToString();
+                Session["tenantID"] = tenantID;
+                if (tenantID != "")
+                {
+                    Response.Redirect("TenantDashboard.aspx");
+                }
+                else if (hostID != "")
+                {
+                    Response.Redirect("HostDashboard.aspx");
+                }
+                else if (adminID != "")
+                {
+                    Response.Redirect("AdminDashboard.aspx");
+                }
+                sc.Close();
+            }
         }
         else
         {
             Response.Redirect("Login.aspx");
         }
+
+        
     }
     protected void translate(object sender, EventArgs e)
     {
--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
 public partial class MasterPage : System.Web.UI.MasterPage
 {
+    SqlConnection sc = new SqlConnection(WebConfigurationManager.ConnectionStrings["RDSConnectionString"].ConnectionString);
+
     protected void Page_Load(object sender, EventArgs e)
     {
         HttpCookie userInfo = new HttpCookie("userInfo");
@@ -24,13 +28,45 @@ public partial class MasterPage : System.Web.UI.MasterPage
     }
     protected void dashboard(object sender, EventArgs e)
     {
-        if (Session["LoggedIn"] == null)
+        if (Session["LoggedIn"].ToString() == "true")
         {
-            Response.Redirect("Login.aspx");
+             
+
+            sc.Open();
+            int accountID = Convert.ToInt32(Session["accountID"]);
+            Response.Write(accountID);
+
+            SqlCommand login = new SqlCommand("SELECT HostID, TenantID, AdminID FROM [Capstone].[dbo].[Login] WHERE AccountID = @AccountID", sc);
+            login.Parameters.AddWithValue("@AccountID", accountID);
+            login.Connection = sc;
+            SqlDataReader rdr = login.ExecuteReader();
+            string tenantID = "", hostID = "", adminID = "";
+            while (rdr.Read())
+            {
+                adminID = rdr["AdminID"].ToString();
+                tenantID = rdr["TenantID"].ToString();
+                hostID = rdr["HostID"].ToString();
+            }
+            Session["hostID"] = hostID;
+            Session["tenantID"] = tenantID;
+            Session["adminID"] = adminID;
+            if (tenantID != "")
+            {
+                Response.Redirect("TenantDashboard.aspx");
+            }
+            else if (hostID != "")
+            {
+                Response.Redirect("HostDashboard.aspx");
+            }
+            else if (adminID != "")
+            {
+                Response.Redirect("AdminDashboard.aspx");
+            }
+            sc.Close();
         }
         else
         {
-            Response.Redirect("TenantDashboard.aspx");
+            Response.Redirect("Login.aspx");
         }
     }
     protected void FAQ(object sender, EventArgs e)
