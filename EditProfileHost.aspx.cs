b73387494b412cf4105586b52f2e5fa6726b63d8
Amanda Cho <choamandak@gmail.com>
2019-12-10 16:11:40 -0500

    edit profile host and tenant comments

--- EditProfileHost.aspx.cs
+++ EditProfileHost.aspx.cs
@@ -11,6 +11,7 @@ using System.Web.UI.WebControls;
 
 public partial class EditProfileHost : System.Web.UI.Page
 {
+    //create database connection
     SqlConnection sc = new SqlConnection(WebConfigurationManager.ConnectionStrings["RDSConnectionString"].ConnectionString);
     int hostID;
     protected void Page_Load(object sender, EventArgs e)
@@ -18,14 +19,22 @@ public partial class EditProfileHost : System.Web.UI.Page
         sc.Open();
         String username = Session["username"].ToString();
 
+        //grab HostID of current user
         SqlCommand insert = new SqlCommand("SELECT HostID FROM [dbo].[Login] WHERE Username = @Username", sc);
         insert.Parameters.AddWithValue("@Username", username);
         insert.Connection = sc;
         hostID = Convert.ToInt32(insert.ExecuteScalar());
         insert.ExecuteNonQuery();
+        
+        //grab host information that will prepopulate textboxes
         SqlCommand filter = new SqlCommand("SELECT Email, PhoneNumber, Firstname, MiddleName, LastName, BirthDate," +
                             "Gender, BackgroundCheckDate, BackgroundCheckResult, LastUpdatedBy, LastUpdated, HostBio FROM [dbo].[Host] WHERE " +
-                            "HostID = " + hostID, sc);
+                            "HostID = @hostID", sc);
+
+        filter.Parameters.AddWithValue("@hostID", hostID);
+        filter.Connection = sc;
+        filter.ExecuteNonQuery();
+        
         if (IsPostBack == false)
         {
             SqlDataReader rdr = filter.ExecuteReader();
@@ -43,25 +52,30 @@ public partial class EditProfileHost : System.Web.UI.Page
 
 
     }
+
+    //update database with new changes
     protected void saveChanges(object sender, EventArgs e)
     {
+        //create database connection
         using (SqlConnection sc = new SqlConnection(ConfigurationManager.ConnectionStrings["RDSConnectionString"].ConnectionString))
         {
             using (SqlCommand update = new SqlCommand())
             {
                 try
                 {
+                    //update host table with new information
                     update.Connection = sc;
                     update.CommandText = "UPDATE [dbo].[Host] SET Email= @Email, PhoneNumber= @PhoneNumber, FirstName= @FirstName," +
-                    "LastName = @LastName, HostBio = @HostBio WHERE HostID = " + hostID;
+                    "LastName = @LastName, HostBio = @HostBio WHERE HostID = @hostID";
                     update.Parameters.AddWithValue("@Email", emailTextbox.Text);
                     update.Parameters.AddWithValue("@PhoneNumber", phoneNumberTextbox.Text);
                     update.Parameters.AddWithValue("@FirstName", firstNameTextbox.Text);
                     update.Parameters.AddWithValue("@LastName", lastNameTextbox.Text);
                     update.Parameters.AddWithValue("@HostBio", HostBioTextbox.Text);
+                    update.Parameters.AddWithValue("@hostID", hostID);
                     sc.Open();
 
-                    // email stuff
+                    //update email
                     String emailNew = emailTextbox.Text;
                     Session["Email"] = emailNew;
                     SqlCommand emailCheck = new SqlCommand("SELECT Count(*) FROM [dbo].[Host] WHERE lower(email) = @Email", sc);
@@ -70,6 +84,7 @@ public partial class EditProfileHost : System.Web.UI.Page
                     int count = Convert.ToInt32(emailCheck.ExecuteScalar());
                     emailCheck.ExecuteNonQuery();
                     
+                    //validate email is available
                     if (count <=1)
                     {
 
@@ -98,17 +113,19 @@ public partial class EditProfileHost : System.Web.UI.Page
                 }
 
             }
+
+            //update login with new password
             using (SqlCommand updateLogin = new SqlCommand())
             {
                 updateLogin.Connection = sc;
-                updateLogin.CommandText = "UPDATE [dbo].[Login] SET Password = @Password WHERE HostID = " + hostID;
+                updateLogin.CommandText = "UPDATE [dbo].[Login] SET Password = @Password WHERE HostID = @hostID";
                 updateLogin.Parameters.AddWithValue("@Password", PasswordHash.HashPassword(passwordTextbox.Text));
+                updateLogin.Parameters.AddWithValue("@hostID", hostID);
 
                 try
                 {
                     sc.Open();
                     updateLogin.ExecuteNonQuery();
-                    //resultmessage.Text = "Profile is updated.";
                 }
                 catch (Exception t)
                 {
